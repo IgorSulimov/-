f3d08b3749b96e16010329d0d1ed0c8766aecad4
IgorSulimov <145384518+IgorSulimov@users.noreply.github.com>
2026-05-07 21:39:02 +0300

    Create Qv_7_Lock_Peterson.c

---
+++ Qv_7_Lock_Peterson.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+volatile int counter = 0;
+volatile int request[2] = { 0, 0 };
+volatile int turn = 0;
+int iterations;
+void lock(int id) {
+	request[id] = 1;
+	int other = 1 - id;
+	turn = other;
+	while (request[other] == 1 && turn == other) {
+	};//spin 
+}
+void unlock(int id) {
+	request[id] = 0;
+}
+void* increment(void* arg) {
+	int id = *(int*)arg;
+	for (int i = 0; i < iterations; i++) {
+		lock(id);
+		counter++;
+		unlock(id);
+	}
+}
+int main(int argc, char* argv[]) {
+	pthread_t one_p, two_p;
+	if (argc != 2) return 1;
+	iterations = atoi(argv[1]);
+	int tid[2];
+	tid[0] = 0;
+	pthread_create(&one_p, NULL, increment, &tid[0]);
+	tid[1] = 1;
+	pthread_create(&two_p, NULL, increment, &tid[1]);
+	pthread_join(one_p, NULL);
+	pthread_join(two_p, NULL);
+	printf("counter = %d \n", counter);
+	return 0;
+}
