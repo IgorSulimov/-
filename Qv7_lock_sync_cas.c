2c8b001b7cfa98fa99697a7257ebfcd5628e7fd9
IgorSulimov <145384518+IgorSulimov@users.noreply.github.com>
2026-05-22 08:07:36 +0300

    Create Qv7_lock_sync_cas.c

---
+++ Qv7_lock_sync_cas.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+volatile int _lock = 0;
+long long all_in_spin = 0;
+int iterations;
+long long counter = 0;
+
+int lock(volatile int *lock) {
+    int spin = 0;
+    while (__sync_val_compare_and_swap(lock, 0, 1) == 1) {
+        spin++;
+    }
+    return spin;
+}
+
+void unlock(volatile int *lock) {
+    *lock = 0;
+}
+
+void* thread_function(void* arg) {
+    long long local_spin_sum = 0;
+    for (int i = 0; i < iterations; i++) {
+        local_spin_sum += lock(&_lock);
+        counter++;
+        unlock(&_lock);
+    }
+    __sync_fetch_and_add(&all_in_spin, local_spin_sum);
+    return NULL;
+}
+
+int main(int argc, char* argv[]) {
+    pthread_t thread1, thread2;
+    
+    if (argc != 2) return 1;
+    iterations = atoi(argv[1]);
+    
+    pthread_create(&thread1, NULL, thread_function, NULL);
+    pthread_create(&thread2, NULL, thread_function, NULL);
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+    
+    printf("counter = %lld\n", counter);
+    printf("all in spin = %lld \n", all_in_spin);
+    return 0;
+}
