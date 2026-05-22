cbbce77558513286b1f25e4e39b1aba971fa2d6e
IgorSulimov <145384518+IgorSulimov@users.noreply.github.com>
2026-05-22 07:50:39 +0300

    Update Qv7_lock_sync_tas.c

--- Qv7_lock_sync_tas.c
+++ Qv7_lock_sync_tas.c
@@ -1,63 +1,42 @@
-#include <unistd.h>
 #include <stdio.h>
 #include <pthread.h>
-#include <linux/futex.h>
-#include <sys/syscall.h>
-int spin_count[2] = { 0, 0 };
 // Глобальная переменная для блокировки
 volatile int _lock = 0;
 volatile int all_in_spin = 0;
-int iterations;
-
-int futex_wait(volatile int* futexp) {
-    return syscall(SYS_futex, futexp, FUTEX_WAIT, 1, NULL, NULL, 0);
-}
-
-void futex_wake(volatile int* futexp) {
-    syscall(SYS_futex, futexp, FUTEX_WAKE, 1, NULL, NULL, 0);
-}
 // Функция для захвата блокировки
-int lock(volatile int* lock,int thread_id) {
-    int spin = 0;
+void lock(volatile int *lock) {
     while (__sync_lock_test_and_set(lock, 1)) {
         // Активное ожидание (busy-waiting), 
-        // пока блокировка не освободится
-        spin++;
-        futex_wait(lock);
+		// пока блокировка не освободится
     }
-    spin_count[thread_id] += spin;
-    return spin;
 }
 // Функция для освобождения блокировки
-void unlock(volatile int* lock) {
-    *lock = 0;// Освобождаем ресурс
-    futex_wake(lock);
+void unlock(volatile int *lock) {
+	*lock = 0;// Освобождаем ресурс
 }
 // Общий ресурс
 int counter = 0;
 // Функция, которую выполняют потоки
 void* thread_function(void* arg) {
-    for (int i = 0; i < iterations; i++) {
-        int id = *(int*)arg;
-        all_in_spin += lock(&_lock,id); // Захватываем блокировку
+    for (int i = 0; i < 1000000000; i++) {
+        all_in_spin += lock(&_lock); // Захватываем блокировку
         counter++;      // Работаем с общим ресурсом
         unlock(&_lock); // Освобождаем блокировку
     }
     return NULL;
 }
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <iterations>\n", argv[0]);
-        return 1;
-    }
-    iterations = atoi(argv[1]);
+int main(int argc, char*argv[]) {
     pthread_t thread1, thread2;
     // Создаем два потока
+    if(argc != 2) {
+        printf("Использование: %s <колтчество_инкрементов_на_поток>\n",argv[0]);
+        return 1;
+    }
     pthread_create(&thread1, NULL, thread_function, NULL);
     pthread_create(&thread2, NULL, thread_function, NULL);
     // Ждем завершения потоков
     pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
+	pthread_join(thread2, NULL);
     // Выводим результат
     printf("counter = %d\n", counter);
     printf("all in spin = %d \n", all_in_spin);
